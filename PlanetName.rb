81b9d7229a5f1a1569a469114bd1e1bf1efdeaa6
KrzysztofBalejko <43317951+KrzysztofBalejko@users.noreply.github.com>
2018-12-17 22:05:56 +0000

    Add files via upload

---
+++ PlanetName.rb
@@ -0,0 +1,20 @@
+def get_planet_name(id)
+    case id
+    when id = 1
+       "Mercury"
+    when id = 2
+       "Venus"
+    when id = 3
+       "Earth"
+    when id = 4
+       "Mars"
+    when id = 5
+       "Jupiter"
+    when id = 6
+       "Saturn"
+    when id = 7
+       "Uranus"
+    when id = 8
+       "Neptune"
+   end
+end
