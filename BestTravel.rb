5f2bc16ddd44c118bf9d0e56a86615dcf4719dde
KrzysztofBalejko <ec2-user@ip-172-31-21-6.eu-west-1.compute.internal>
2018-12-18 21:07:20 +0000

    Code refactoring + test

--- BestTravel.rb
+++ BestTravel.rb
@@ -1,6 +1,21 @@
 def choose_best_sum(t, k, ls)
-    ls.combination(k)
-      .map{|path| path.inject(:+)}
-      .select{|sum| sum <= t}
-      .max
+  ls.combination(k).map{|path| path.inject(:+)}.select{|sum| sum <= t}.max
 end
+
+
+# The above will passing below test
+
+=begin 
+Test.describe("choose_best_sum") do
+    Test.it("Basic Tests") do
+        ts = [50, 55, 56, 57, 58]
+        Test.assert_equals(choose_best_sum(163, 3, ts), 163)
+        ts = [50]
+        Test.assert_equals(choose_best_sum(163, 3, ts), nil)
+        ts = [91, 74, 73, 85, 73, 81, 87]
+        Test.assert_equals(choose_best_sum(230, 3, ts), 228)
+    end
+end
+=end
+
+
