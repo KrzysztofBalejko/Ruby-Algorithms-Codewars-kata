8e0d4b4e197cc04d1bff6d07c8b27c9077731ef8
KrzysztofBalejko <ec2-user@ip-172-31-21-6.eu-west-1.compute.internal>
2018-12-22 10:25:36 +0000

    Refactoring

--- PixelArtPlanning.rb
+++ PixelArtPlanning.rb
@@ -1,8 +1,15 @@
 def is_divisible(wall_length, pixel_size)
-
-if wall_length % pixel_size == 0
-  return true
-elsif wall_length % pixel_size != 0
-  return false
-  end
+  wall_length % pixel_size == 0 ? true : false
 end
+
+#The above has passed all the tests
+
+=begin
+describe "Basic tests" do
+  Test.assert_equals(is_divisible(4050, 27), true)
+  Test.assert_equals(is_divisible(4066, 27), false)
+  Test.assert_equals(is_divisible(10000, 20), true)
+  Test.assert_equals(is_divisible(10005, 20), false)
+  Test.assert_equals(is_divisible(10005, 1), true)
+end
+=end
