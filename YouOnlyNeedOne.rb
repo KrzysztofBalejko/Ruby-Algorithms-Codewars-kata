ee9d4fa27114b2c272fb8c00386ff237129e8697
KrzysztofBalejko <ec2-user@ip-172-31-21-6.eu-west-1.compute.internal>
2018-12-22 11:11:55 +0000

    Code refactoring - more like English

--- YouOnlyNeedOne.rb
+++ YouOnlyNeedOne.rb
@@ -1,7 +1,21 @@
 def check(arr,element)
-  if arr.include? element
-    return true
-  else
-    return false
-  end
+  return true if arr.include? element 
+  false
 end
+
+#The above has passed all the tests
+
+=begin
+describe "Basic tests" do
+  Test.assert_equals(check([66, 101], 66), true)
+  Test.assert_equals(check([78, 117, 110, 99, 104, 117, 107, 115], 8), false)
+  Test.assert_equals(check([101, 45, 75, 105, 99, 107], 107), true)
+  Test.assert_equals(check(["t", "e", "s", "t"], "e"), true)
+  Test.assert_equals(check(["what", "a", "great", "kata"], "kat"), false)
+  Test.assert_equals(check([66, "codewars", 11, "alex loves pushups"], "alex loves pushups"), true)
+  Test.assert_equals(check(["come", "on", 110, "2500", 10, "!", 7, 15], "Come"), false)
+  Test.assert_equals(check(["when's", "the", "next", "Katathon?", 9, 7], "Katathon?"), true)
+  Test.assert_equals(check([8, 7, 5, "bored", "of", "writing", "tests", 115], 45), false)
+  Test.assert_equals(check(["anyone", "want", "to", "hire", "me?"], "me?"), true)
+end
+=end
